50308949575554ce7fc659329c71755fbd070d0a
nemalo <75143546+nemalo@users.noreply.github.com>
2021-12-05 22:47:36 +0100

    Additional code fixes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Weather from "./Weather";
 import "./styles.css";
 import "./Weather.css";
 
