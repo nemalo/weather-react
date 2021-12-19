0432a562fc429a82893d4b0a3d5cbb89b7cc5cac
nemalo <75143546+nemalo@users.noreply.github.com>
2021-12-19 16:04:59 +0100

    Trying to see weekly forecast
    Something in my code is still absolutely broken

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -17,15 +17,24 @@ export default function WeatherForecast(props) {
       <div className="WeatherInfo">
         <div className="row weekly-forecast" id="weekly-weather-forecast">
           {forecast.map(function (dailyForecast, index) {
-            return (
-              <span key={index}>
-                <WeatherForecastDay data={dailyForecast} />
-              </span>
-            );
+            if (index < 6) {
+              return (
+                <span key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </span>
+              );
+            }
           })}
         </div>
       </div>
     );
+  } else {
+    let apiKey = "57821c3b75b60c68ecd1a8d0dd1aa8d3";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude={part}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
   }
 
   let apiKey = "57821c3b75b60c68ecd1a8d0dd1aa8d3";
